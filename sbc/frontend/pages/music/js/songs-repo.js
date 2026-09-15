38b051af02c39f67610cb11eb43a2563e5acacc5
Your Name <your.email@example.com>
2026-09-15 08:18:53 -0300

    el pollito pio

---
+++ sbc/frontend/pages/music/js/songs-repo.js
@@ -0,0 +1,105 @@
+export const SONGS = [
+    {
+        title: "El polito pio",
+        author: "Bruno Benvenuti",
+        audio: "audio/PULCINO PIO - El Pollito Pio.mp3",
+        cover: "covers/pollito pio.png",
+        lyrics: `
+[00:00.76] En la radio hay un pollito, En la radio hay un pollito,
+[00:04.74] Y el pollito PIO,
+[00:05.88] El pollito PIO, El pollito PIO,
+[00:07.97] El pollito PIO, El pollito PIO, El pollito PIO.
+[00:11.15] En la radio hay una gallina, En la radio hay una gallina,
+[00:14.96] Y la gallina COO,
+[00:15.77] Y el pollito PIO,
+[00:16.95] El pollito PIO, El pollito PIO, El pollito PIO, El pollito PIO.
+[00:21.47] En la radio hay también un gallo, En la radio hay también un gallo,
+[00:25.13] Y el gallo COCOROCO,
+[00:26.29] Y la gallina COO,
+[00:27.03] Y el pollito PIO,
+[00:28.46] El pollito PIO, El pollito PIO, El pollito PIO.
+[00:31.85] En la radio hay un pavo, En la radio hay un pavo,
+[00:35.69] Y el pavo GLÚ GLÚ GLÚ,
+[00:36.64] Y el gallo COCOROCO,
+[00:37.43] Y la gallina COO,
+[00:38.54] Y el pollito PIO,
+[00:39.31] El pollito PIO, El pollito PIO.
+[00:42.08] En la radio hay una paloma, En la radio hay una paloma,
+[00:45.57] Y la paloma RUU,
+[00:46.53] El pavo GLÚ GLÚ GLÚ,
+[00:47.63] El gallo COCOROCO,
+[00:48.73] Y la gallina COO,
+[00:49.51] Y el pollito PIO,
+[00:50.39] El pollito PIO, El pollito PIO, El pollito PIO.
+[00:54.42] En la radio hay también un gato, En la radio hay también un gato,
+[00:58.14] Y el gato MIAO,
+[00:58.98] La paloma RUU,
+[01:00.06] El pavo GLÚ GLÚ GLÚ
+[01:01.02] El gallo COCOROCO,
+[01:02.42] La gallina COO,
+[01:03.28] Y el pollito PIO,
+[01:04.29] El pollito PIO, El pollito PIO.
+[01:05.90] En la radio hay también un perro, En la radio hay también un perro,
+[01:09.94] Y el perro GUAU GUAU,
+[01:10.96] El gato MIAO,
+[01:12.07] La paloma RUU,
+[01:13.19] El pavo GLÚ GLÚ GLÚ,
+[01:13.92] El gallo COCOROCO,
+[01:14.98] La gallina COO,
+[01:16.09] Y el pollito PIO,
+[01:17.16] El pollito PIO, El pollito PIO, El pollito PIO.
+[01:20.34] En la radio hay una cabra, En la radio hay una cabra,
+[01:24.04] Y la cabra MEEE,
+[01:25.35] El perro GUAU GUAU,
+[01:26.47] El gato MIAO,
+[01:27.56] La paloma RUU,
+[01:28.59] El pavo GLÚ GLÚ GLÚ,
+[01:29.69] El gallo COCOROCO,
+[01:30.44] La gallina COO,
+[01:31.56] Y el pollito PIO,
+[01:32.51] El pollito PIO, El pollito PIO.
+[01:35.08] En la radio hay un cordero, En la radio hay un cordero,
+[01:39.68] Y el cordero BEEE,
+[01:40.41] La cabra MEEE,
+[01:41.57] El perro GUAU GUAU,
+[01:42.30] El gato MIAO,
+[01:43.45] La paloma RUU,
+[01:44.25] El pavo GLÚ GLÚ GLÚ,
+[01:45.36] El gallo COCOROCO,
+[01:46.37] La gallina COO,
+[01:47.48] Y el pollito PIO,
+[01:48.25] El pollito PIO, El pollito PIO, El pollito PIO.
+[01:51.78] En la radio hay una vaca, En la radio hay una vaca,
+[01:55.78] Y la vaca MOO,
+[01:56.59] El cordero BEEE,
+[01:57.84] La cabra MEEE,
+[01:58.93] El perro GUAU GUAU,
+[01:59.79] El gato MIAO,
+[02:00.86] La paloma RUU,
+[02:01.62] El pavo GLÚ GLÚ GLÚ,
+[02:02.67] El gallo COCOROCO,
+[02:03.42] La gallina COO,
+[02:04.56] Y el pollito PIO
+[02:05.60] El pollito PIO, El pollito PIO, El pollito PIO, El pollito PIO.
+[02:09.87] En la radio hay también un toro, En la radio hay también un toro,
+[02:13.83] Y el toro MUU,
+[02:14.96] La vaca MOO,
+[02:16.26] El cordero BEEE,
+[02:17.02] La cabra MEEE,
+[02:18.20] El perro GUAU GUAU,
+[02:19.23] El gato MIAO,
+[02:20.02] La paloma RUU,
+[02:21.08] El pavo GLÚ GLÚ GLÚ,
+[02:22.12] El gallo COCOROCO
+[02:23.24] Y la gallina COO,
+[02:24.24] Y el pollito PIO
+[02:25.04] El pollito PIO, El pollito PIO, El pollito PIO,
+[02:28.59] En la radio hay un tractor, En la radio hay un tractor,
+[02:32.40] Y el tractor BRUUM,
+[02:33.49] Y el tractor BRUUM,
+[02:34.47] Y el tractor BRUUM,
+[02:35.56] Y el pollito (puaggg) OH OH
+[02:37.94] 
+        `
+    },
+];
